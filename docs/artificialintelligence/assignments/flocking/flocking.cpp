2f58809b786e406ebc9d1834aa521efaf06bf821
jaydchamp <jayden.lombardi@mymail.champlain.edu>
2024-09-08 16:07:50 -0400

    finished creating and implementing Cohesion, Alignment, and Separation structs

--- docs/artificialintelligence/assignments/flocking/flocking.cpp
+++ docs/artificialintelligence/assignments/flocking/flocking.cpp
@@ -100,8 +100,46 @@ struct Cohesion {
 
   Cohesion() = default;
 
-  Vector2 ComputeForce(const vector<Boid>& boids, int boidAgentIndex) {
-    return {};
+  Vector2 ComputeForce(const vector<Boid>& boids, int boidAgentIndex)
+  {
+    Vector2 centerOfMass = {0,0};
+    int numNeighbours = 0;
+
+    for (int i = 0; i < boids.size(); ++i)
+    {
+      if (i != boidAgentIndex)
+      {
+        double distance = boids[boidAgentIndex].position.Distance(boids[i].position);
+        if(distance <= radius)
+        {
+          centerOfMass += boids[i].position;
+          ++numNeighbours;
+        }
+      }
+    }
+
+    //if no neighbors return zero force
+    if(numNeighbours == 0)
+    {
+      return Vector2(0,0);
+    }
+
+    //calculate average pos
+    centerOfMass /= numNeighbours;
+
+    //calculate velocity
+    Vector2 directionToCenter = centerOfMass - boids[boidAgentIndex].position;
+    double distanceToCenter = directionToCenter.getMagnitude();
+
+    //compute force (cant go over radius limit)
+    double forceMagnitude = k * min(distanceToCenter, radius) / radius;
+    Vector2 force = directionToCenter.normalized() * forceMagnitude;
+
+    cout << "Cohesion - Center of Mass: " << centerOfMass.x << " " << centerOfMass.y << endl;
+    cout << "Cohesion - Direction to Center: " << directionToCenter.x << " " << directionToCenter.y << endl;
+    cout << "Cohesion - Force Magnitude: " << forceMagnitude << endl;
+    cout << "Cohesion - Force: " << force.x << " " << force.y << endl;
+    return force;
   }
 };
 
@@ -111,8 +149,49 @@ struct Alignment {
 
   Alignment() = default;
 
-  Vector2 ComputeForce(const vector<Boid>& boids, int boidAgentIndex) {
-    return {};
+  Vector2 ComputeForce(const vector<Boid>& boids, int boidAgentIndex)
+  {
+    Vector2 avgVelocity = {0,0};
+    int numNeighbours = 0;
+
+    for (int i = 0; i < boids.size(); ++i)
+    {
+      if(i != boidAgentIndex)
+      {
+        double distance = boids[boidAgentIndex].position.Distance(boids[i].position);
+
+        if(distance <= radius)
+        {
+          avgVelocity += boids[i].velocity;
+          ++numNeighbours;
+        }
+      }
+    }
+
+    //include velocity of the agent itself
+    avgVelocity += boids[boidAgentIndex].velocity;
+    ++numNeighbours;
+
+    //compute force with average velocity
+    if(numNeighbours > 0)
+    {
+      //average velocity
+      avgVelocity /= numNeighbours;
+      Vector2 direction = avgVelocity.normalized();
+      Vector2 currentVelocity = boids[boidAgentIndex].velocity.normalized();
+
+      //compute force
+      Vector2 alignForce = (direction - currentVelocity) * k;
+
+      cout << "Alignment - Avg Velocity: " << avgVelocity.x << " " << avgVelocity.y << endl;
+      cout << "Alignment - Current Velocity: " << currentVelocity.x << " " << currentVelocity.y << endl;
+      cout << "Alignment - Alignment Force: " << alignForce.x << " " << alignForce.y << endl;
+
+      return alignForce;
+    }
+
+    //no force if no neighbors
+    return Vector2(0,0);
   }
 };
 
@@ -123,8 +202,52 @@ struct Separation {
 
   Separation() = default;
 
-  Vector2 ComputeForce(const vector<Boid>& boids, int boidAgentIndex) {
-    return {};
+  Vector2 ComputeForce(const vector<Boid>& boids, int boidAgentIndex)
+  {
+    Vector2 separationForce = {0, 0};
+    Vector2 agentPos = boids[boidAgentIndex].position;
+    int numNeighbours = 0;
+
+    //go through all agents
+    for (int i = 0; i < boids.size(); ++i)
+    {
+      //if its not the chosen agent
+      if(i != boidAgentIndex)
+      {
+        //and they are within the radius
+        double distance = agentPos.Distance(boids[i].position);
+
+        if(distance < radius)
+        {
+          //compute separation forces
+          Vector2 directionToNeighbor = agentPos - boids[i].position;
+          Vector2 force = directionToNeighbor / (distance * distance); //changed to distance squared
+          separationForce += force;
+          ++numNeighbours;
+        }
+      }
+    }
+
+    //if there are any neighbors within the radius compute final force
+    if(numNeighbours > 0)
+    {
+      //average force
+      separationForce /= numNeighbours;
+      double forceMagnitude = k * separationForce.getMagnitude();
+
+      if(forceMagnitude > maxForce)
+      {
+        //clamp force
+        separationForce = separationForce.normalized() * maxForce;
+      }
+      else
+      {
+        //scale by coefficient
+        separationForce *= k;
+      }
+    }
+    cout << "Separation - Separation Force: " << separationForce.x << " " << separationForce.y << endl;
+    return separationForce;
   }
 };
 
@@ -137,20 +260,19 @@ int main() {
   int numberOfBoids;
   string line; // for reading until EOF
   vector<Boid> currentState, newState;
+
   // Input Reading
   cin >> cohesion.radius >> separation.radius >> separation.maxForce >> alignment.radius >> cohesion.k >> separation.k >> alignment.k >> numberOfBoids;
   for (int i = 0; i < numberOfBoids; i++) {
     Boid b;
     cin >> b.position.x >> b.position.y >> b.velocity.x >> b.velocity.y;
-    //cout << "b.y: " << b.y << endl;
     currentState.push_back(b);
     newState.push_back(b);
   }
   cin.ignore(256, '\n');
-  // Final input reading and processing
-  // todo: edit this. probably my code will be different than yours.
+
   while (getline(cin, line)) { // game loop
-    // Use double buffer! you should read from the current and store changes in the new state.
+    //read from the current and store changes in the new state.
     currentState = newState;
     double deltaT = stod(line);
     // a vector of the sum of forces for each boid.
@@ -158,7 +280,16 @@ int main() {
     // Compute Forces
     for (int i = 0; i < numberOfBoids; i++)  // for every boid
     {
-      for (int j = 0; j < numberOfBoids; j++)  // for every boid combination. Pre-processing loop.
+      // Calculate Cohesion Force
+      Vector2 cohesionForce = cohesion.ComputeForce(currentState, i);
+      // Calculate Separation Force
+      Vector2 separationForce = separation.ComputeForce(currentState, i);
+      // Calculate Alignment Force
+      Vector2 alignmentForce = alignment.ComputeForce(currentState, i);
+
+      // Accumulate the forces
+      allForces[i] += cohesionForce + separationForce + alignmentForce;
+      /*for (int j = 0; j < numberOfBoids; j++)  // for every boid combination. Pre-processing loop.
       {
         // Process Cohesion Forces
         auto dist = (currentState[i].position-currentState[j].position).getMagnitude();
@@ -173,7 +304,7 @@ int main() {
         if (i != j && dist <= alignment.radius) {
           allForces[i] += alignment.ComputeForce(currentState, i);
         }
-      }
+      }*/
     }
     // Tick Time and Output
     // todo: edit this. probably my code will be different than yours.
